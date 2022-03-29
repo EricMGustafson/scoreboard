6546e9878b53dc296c340d1e00f7d812c4b8f0e7
EricMGustafson <emgustafson22@gmail.com>
2022-03-29 00:32:53 -0600

    mo functions

--- main.js
+++ main.js
@@ -5,71 +5,95 @@ drawScore()
 
 
 
+let teams = {}
+  teams = {
+    "Apple": {
+      name: 'Apple',
+      score: 0
+      },
+    "Banana": {
+        name: 'Banana',
+        score: 0
+      }
+  }
 
-function teamOneScore() {
-  score += 1
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
 
-function teamOneScoreThree() {
-  score += 3
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
+  function scoring(team, value) {
+    if (team == 'Apple') {
+      teams.Apple.score += value
+      document.getElementById('score-1').innerText = JSON.stringify(teams.Apple.score)
+    } else if (team == 'Banana') {
+      teams.Banana.score += value
+      document.getElementById('score-2').innerText =JSON.stringify(teams.Banana.score)
+    }
+  }
 
-function teamOneScoreSix() {
-  score += 6
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
+//
 
-function teamOneMinus() {
-  score -= 1
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
+// function teamOneScore() {
+//   score += 1
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamOneMinusThree() {
-  score -= 3
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
+// function teamOneScoreThree() {
+//   score += 3
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamOneMinusSix() {
-  score -= 6
-  document.getElementById('score-1').innerText = JSON.stringify(score)
-}
+// function teamOneScoreSix() {
+//   score += 6
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamTwoScore() {
-  scoreTwo += 1
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamOneMinus() {
+//   score -= 1
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamTwoScoreThree() {
-  scoreTwo += 3
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamOneMinusThree() {
+//   score -= 3
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamTwoScoreSix() {
-  scoreTwo += 6
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamOneMinusSix() {
+//   score -= 6
+//   document.getElementById('score-1').innerText = JSON.stringify(score)
+// }
 
-function teamTwoMinus() {
-  scoreTwo -= 1
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamTwoScore() {
+//   scoreTwo += 1
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
 
-function teamTwoMinusThree() {
-  scoreTwo -= 3
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamTwoScoreThree() {
+//   scoreTwo += 3
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
 
-function teamTwoMinusSix() {
-  scoreTwo -= 6
-  document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
-}
+// function teamTwoScoreSix() {
+//   scoreTwo += 6
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
+
+// function teamTwoMinus() {
+//   scoreTwo -= 1
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
+
+// function teamTwoMinusThree() {
+//   scoreTwo -= 3
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
+
+// function teamTwoMinusSix() {
+//   scoreTwo -= 6
+//   document.getElementById('score-2').innerText = JSON.stringify(scoreTwo)
+// }
 
 
 function drawScore() {
-  let oldScore = document.getElementById('score-1')
-  let oldScoreTwo = document.getElementById('score-2')
+  let oldScore = JSON.parse(document.getElementById('score-1').innerText)
+  let oldScoreTwo = JSON.parse(document.getElementById('score-2').innerText)
   oldScore.innerText += score
   oldScoreTwo.innerText += scoreTwo
 }
@@ -92,13 +116,13 @@ function gameReset() {
 
 
 
-function whoWinning() {
-    if (score > scoreTwo) {
-        document.getElementById('winning').innerText = 'One'
-      } else {
-          document.getElementById('winning').innerText = 'Two'
-        }
-      }
+// function whoWinning() {
+//     if (score > scoreTwo) {
+//         document.getElementById('winning').innerText = 'One'
+//       } else {
+//           document.getElementById('winning').innerText = 'Two'
+//         }
+//       }
       
       // function gameOver() {
       //     if (score == 25 || score == 25) {}
